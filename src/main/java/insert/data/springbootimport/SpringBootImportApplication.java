e2432a4f4cf9447bace6ea8adbff01f43ab03e39
CodigoFacilOficial <minutocodefacil@gmail.com>
2023-10-28 22:43:45 -0400

    add proyect spring

---
+++ src/main/java/insert/data/springbootimport/SpringBootImportApplication.java
@@ -0,0 +1,13 @@
+package insert.data.springbootimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootImportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/insert/data/springbootimport/SpringBootImportApplicationTests.java
@@ -0,0 +1,13 @@
+package insert.data.springbootimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
